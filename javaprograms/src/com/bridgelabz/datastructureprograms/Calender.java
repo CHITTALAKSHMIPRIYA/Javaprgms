f854ee325cae99b236e7c9b922a555e64c60ce35
CHITTALAKSHMIPRIYA <lakshmichitta96@gmail.com>
2018-05-28 13:26:32 +0530

    javaprograms

---
+++ javaprograms/src/com/bridgelabz/datastructureprograms/Calender.java
@@ -0,0 +1,34 @@
+
+package com.bridgelabz.datastructureprograms;
+
+import com.bridgelabz.util.Utility;
+
+public class Calender
+{
+	    public static void main(String[] args) 
+	    {	System.out.println("enter month");
+	        System.out.println("enter year");
+	        int month = Utility.Int();
+	        int year = Utility.Int();
+	        String[] months = {"","January", "February", "March","April", "May", "June",
+	            "July", "August", "September","October", "November", "December"};
+
+	        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	        if (month == 2 && Utility.leap(year)) 
+	        {
+	        	days[month] = 29;
+	        }
+	   System.out.println("   " + months[month] + " " + year);
+	        System.out.println(" S  M Tu  W Th  F  S");
+	        int d = Utility.dayOfWeek1(month,1,year);
+	        for (int i = 0; i < d; i++)
+	        {
+	            System.out.print("   ");
+	        }
+	        for (int i = 1; i <= days[month]; i++)
+	        {
+	            System.out.printf("%2d ", i);
+	            if (((i + d) % 7 == 0) || (i == days[month])) 
+	            	System.out.println();
+	        } }
+}
--- javaprograms/src/com/bridgelabz/util/Utility.java
+++ javaprograms/src/com/bridgelabz/util/Utility.java
@@ -102,17 +102,17 @@ public class Utility {
 	 */
 	public static int dayOfWeek(int month,int day,int year)
 	{
-		System.out.println("entered date is"+month+"/"+day+"/"+year);
+	System.out.println("entered date is"+month+"/"+day+"/"+year);
 	int	y0 = year - (14 - month) / 12;
 	System.out.println(y0);
 	int x = y0 + y0/4 - y0/100 + y0/400;
 	System.out.println(x);
-int	m0 = month + 12 * ((14 -month) / 12) -2;
-System.out.println(m0);
-int d0 = (day + x + 31*m0/ 12) % 7;
-System.out.println(d0);
-return d0;
-}
+    int	m0 = month + 12 * ((14 -month) / 12) -2;
+    System.out.println(m0);
+    int d0 = (day + x + 31*m0/ 12) % 7;
+    System.out.println(d0);
+    return d0;
+     }
 	
 	/**
 	 * function to calculate fahrenheit
@@ -639,7 +639,7 @@ public static boolean Boolean()
  */
 public  static boolean leap(int year)
 {
-if(year>1000)
+if(year>1582)
 {
 	if((year%400==0)||(year%4==0&&year%100!=0))
 	{
@@ -877,13 +877,21 @@ public static int sumisZero(int[] a,int length)
 			return false;
 		}
 	}
-	
+	//calender
+public static int dayOfWeek1(int month,int day,int year)
+{
+//System.out.println("entered date is"+month+"/"+day+"/"+year);
+int	y0 = year - (14 - month) / 12;
+//System.out.println(y0);
+int x = y0 + y0/4 - y0/100 + y0/400;
+//System.out.println(x);
+int	m0 = month + 12 * ((14 -month) / 12) -2;
+//System.out.println(m0);
+int d0 = (day + x + 31*m0/ 12) % 7;
+//System.out.println(d0);
+return d0;
+ }
 }
-
-//
-
-
-
 	
 
 
